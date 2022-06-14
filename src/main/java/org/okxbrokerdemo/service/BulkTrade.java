bd6414ca8901c5ef95aa33501e9d1aa02306bd3a
crystalcat <dev@tinykittens.dev>
2022-06-13 21:59:15 -0400

    Bulk trade complete

--- src/main/java/org/okxbrokerdemo/service/BulkTrade.java
+++ src/main/java/org/okxbrokerdemo/service/BulkTrade.java
@@ -2,6 +2,9 @@ package org.okxbrokerdemo.service;
 
 import org.okxbrokerdemo.utils.APIKeyHolder;
 
+import java.io.IOException;
+import java.util.Map;
+
 public class BulkTrade {
     private APIKeyHolder apiKeyHolder;
     private String baseURL = "https://www.okx.com";
@@ -13,6 +16,89 @@ public class BulkTrade {
 
     public void setApiKeyHolder(APIKeyHolder apiKeyHolder) {
         this.apiKeyHolder = apiKeyHolder;
+    }
+
+    public Map<String,Object> counterParties(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("GET","/api/v5/rfq/counterparties",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> createRfq(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/create-rfq",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> cancelRfq(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/cancel-rfq",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> cancelBatchRfq(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/cancel-batch-rfqs",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> cancelAllRfq(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/cancel-all-rfqs",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> executeQuote(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/execute-quote",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> createQuote(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/create-quote",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> cancelQuote(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/cancel-quote",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> cancelBatchQuote(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/cancel-batch-quotes",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> cancelAllQuote(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("POST","/api/v5/rfq/cancel-all-quotes",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> getRfqs(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("GET","/api/v5/rfq/rfqs",apiRequestPayload,isSimluate);
+        return  result;
+    }
+    public Map<String,Object> getQuotes(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("GET","/api/v5/rfq/quotes",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> getTrades(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("GET","/api/v5/rfq/trades",apiRequestPayload,isSimluate);
+        return  result;
+    }
+
+    public Map<String,Object> getPublicTrades(APIRequestPayload apiRequestPayload, boolean isSimluate) throws IOException {
+        CommonAPICaller<APIRequestPayload,Map<String,Object>> commonAPICaller = new CommonAPICaller<>(baseURL,apiKeyHolder);
+        Map<String,Object> result = commonAPICaller.requestAPI("GET","/api/v5/rfq/public-trades",apiRequestPayload,isSimluate);
+        return  result;
     }
 
 
