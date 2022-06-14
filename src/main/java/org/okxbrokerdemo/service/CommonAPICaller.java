d961af35f66d1c944bd27df91ee44fb35e11083b
bowen <bowen.zhou@okg.com>
2022-06-14 23:44:39 +0800

    add new requestAPI Method in CommonAPICaller

--- src/main/java/org/okxbrokerdemo/service/CommonAPICaller.java
+++ src/main/java/org/okxbrokerdemo/service/CommonAPICaller.java
@@ -6,7 +6,7 @@ import com.google.gson.reflect.TypeToken;
 import org.okxbrokerdemo.utils.APIKeyHolder;
 import org.okxbrokerdemo.utils.AutorizationMethod;
 import org.okxbrokerdemo.utils.SignatureGenerator;
-import org.okxbrokerdemo.utils.headerMapBuilder;
+import org.okxbrokerdemo.utils.HeaderMapBuilder;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -14,7 +14,6 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.time.Instant;
 import java.util.Map;
@@ -28,6 +27,7 @@ public class CommonAPICaller<E extends APIRequestPayload,R>  {
 
     public CommonAPICaller(String baseUrl,APIKeyHolder apiKeyHolder){
         retrofit = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(ScalarsConverterFactory.create()).build();
+
         requestHandler = retrofit.create(CommonRequestRetrofit.class);
         this.apiKeyHolder = apiKeyHolder;
        // Class cls = HashMap.class;
@@ -44,6 +44,7 @@ public class CommonAPICaller<E extends APIRequestPayload,R>  {
 
         return null;
     }
+
     public R requestAPI(String method,String path,E element,boolean isSimluate) throws IOException {
         String payload = element.getPayLoadJson();
         String timeStamp = Instant.now().toString();
@@ -51,7 +52,7 @@ public class CommonAPICaller<E extends APIRequestPayload,R>  {
         Type retTyp = new TypeToken<R>() { }.getType();
         if(apiKeyHolder.getAutorizationMethod().equals(AutorizationMethod.APIKeyPair) && method.equals("POST")){
             String sign = SignatureGenerator.Generate(timeStamp,method,payload,path,apiKeyHolder.getSecretKey());
-            headers = headerMapBuilder.build(apiKeyHolder.getApiKey(),sign,timeStamp, apiKeyHolder.getPassPhrase(),isSimluate);
+            headers = HeaderMapBuilder.build(apiKeyHolder.getApiKey(),sign,timeStamp, apiKeyHolder.getPassPhrase(),isSimluate);
             Call<String> requestCall = requestHandler.commonPostRequest(path,headers,payload);
             Response<String> response= requestCall.execute();
 
@@ -65,7 +66,7 @@ public class CommonAPICaller<E extends APIRequestPayload,R>  {
         }
 
         if(apiKeyHolder.getAutorizationMethod().equals(AutorizationMethod.AccessToken) && method.equals("POST")){
-            headers = headerMapBuilder.build(apiKeyHolder.getAccessToken(),isSimluate);
+            headers = HeaderMapBuilder.build(apiKeyHolder.getAccessToken(),isSimluate);
             Call<String> requestCall = requestHandler.commonPostRequest(path,headers,payload);
             Response<String> response= requestCall.execute();
             if(response.isSuccessful()){
@@ -93,7 +94,7 @@ public class CommonAPICaller<E extends APIRequestPayload,R>  {
             }
 
             String sign = SignatureGenerator.Generate(timeStamp,method,"",path,apiKeyHolder.getSecretKey());
-            headers = headerMapBuilder.build(apiKeyHolder.getApiKey(),sign,timeStamp, apiKeyHolder.getPassPhrase(),isSimluate);
+            headers = HeaderMapBuilder.build(apiKeyHolder.getApiKey(),sign,timeStamp, apiKeyHolder.getPassPhrase(),isSimluate);
             Call<String> requestCall = requestHandler.commonGetRequest(path,headers);
             Response<String> response= requestCall.execute();
             if(response.isSuccessful()){
@@ -103,7 +104,7 @@ public class CommonAPICaller<E extends APIRequestPayload,R>  {
         }
 
         if(apiKeyHolder.getAutorizationMethod().equals(AutorizationMethod.AccessToken) && method.equals("GET")){
-            headers = headerMapBuilder.build(apiKeyHolder.getAccessToken(),isSimluate);
+            headers = HeaderMapBuilder.build(apiKeyHolder.getAccessToken(),isSimluate);
             Map<String,Object> queryMap = new Gson().fromJson(payload,Map.class);
             if(queryMap.size()>0){
                 path =path + "?";
@@ -123,6 +124,46 @@ public class CommonAPICaller<E extends APIRequestPayload,R>  {
         }
 
 
+        return null;
+    }
+
+    public R requestAPI(String method,String path,E element,boolean isSimluate, Class<R> clazz) throws IOException {
+        String payload = element.getPayLoadJson();
+        String timeStamp = Instant.now().toString();
+        Map<String, String> headers;
+        if (apiKeyHolder.getAutorizationMethod().equals(AutorizationMethod.APIKeyPair) && "POST".equals(method)) {
+            String sign = SignatureGenerator.Generate(timeStamp, method, payload, path, apiKeyHolder.getSecretKey());
+            headers = HeaderMapBuilder.build(apiKeyHolder.getApiKey(), sign, timeStamp, apiKeyHolder.getPassPhrase(), isSimluate);
+            Call<String> requestCall = requestHandler.commonPostRequest(path, headers, payload);
+            Response<String> response = requestCall.execute();
+
+            if (response.isSuccessful()) {
+                return new Gson().fromJson(response.body(), clazz);
+            }
+
+            return new Gson().fromJson(response.errorBody().string(), clazz);
+        }
+
+        //handleGetRequest
+        if(apiKeyHolder.getAutorizationMethod().equals(AutorizationMethod.APIKeyPair) && "GET".equals(method)){
+            Map<String,Object> queryMap = new Gson().fromJson(payload,Map.class);
+            if(queryMap.size()>0){
+                // TODO 使用StringBuilder append 实现字符串扩展
+                path =path + "?";
+                for(String key:queryMap.keySet()){
+                    path = path + key + "=" + queryMap.get(key).toString() + "&";
+                }
+                path = path.substring(0,path.length() - 1);
+            }
+            String sign = SignatureGenerator.Generate(timeStamp,method,"",path,apiKeyHolder.getSecretKey());
+            headers = HeaderMapBuilder.build(apiKeyHolder.getApiKey(),sign,timeStamp, apiKeyHolder.getPassPhrase(),isSimluate);
+            Call<String> requestCall = requestHandler.commonGetRequest(path,headers);
+            Response<String> response= requestCall.execute();
+            if(response.isSuccessful()){
+                return new Gson().fromJson(response.body(),clazz);
+            }
+            return new Gson().fromJson(response.errorBody().string(),clazz);
+        }
         return null;
     }
 
